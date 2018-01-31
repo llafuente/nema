7abebcb1e817764b86d774599966a5473f81ac66
llafuente <llafuente@noboxout.com>
2018-01-31 13:44:20 +0100

    fix: strict cast null to null

--- templates/Cast.ts
+++ templates/Cast.ts
@@ -1,12 +1,18 @@
 export class Cast {
   static number(num): number {
+    if (num === null) return null;
+
     return parseFloat(num);
   }
   static integer(num): number {
+    if (num === null) return null;
+
     return parseInt(num, 10);
   }
 
   static array(arr): any[] {
+    if (arr === null) return null;
+
     if (Array.isArray(arr)) {
       return arr;
     }
@@ -16,10 +22,14 @@ export class Cast {
   }
 
   static boolean(b): boolean {
+    if (b === null) return null;
+
     return b === true;
   }
 
   static string(str): string {
+    if (str === null) return null;
+
     return "" + str;
   }
 }

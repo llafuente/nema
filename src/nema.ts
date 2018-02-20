c0b41e37f3527c776d2a069fac316151952b2704
llafuente <llafuente@noboxout.com>
2018-02-20 12:22:25 +0100

    feat: add --dst param, improve help

--- src/nema.ts
+++ src/nema.ts
@@ -32,68 +32,85 @@ function yellow(text) {
 }
 
 
-/*
-node .\bin\js api --yml=C:\bbva\starter\nova-cli\tmp\initiativesapi.yml
-node .\bin\js api --yml=C:\bbva\autohedgergui\autohedgergui\thin2-fe\src\api\AlgoFramework.yaml
-*/
 program
   .version(packageJSON.version)
   .description("Code generation from swagger")
   .option("--angular5-api", "TARGET: Generate an Angular 5 Module Api client")
-  .option("--mongoose", "TARGET: Generate Mongoose models and CRUD classes")
+  .option("--mongoose", "TARGET: Generate Mongoose Schema, Models & Repositories")
   .option("--express", "TARGET: Generate Express app/routes")
   .option("--override-models", "Override all models while agreggating")
   .option("--override-methods", "Override all methods while agreggating")
-  .option("--lint", "Lint output, this may take a while")
-  .option("--swagger [path]", "Path to swagger yml, repeat to aggregate", function (val, memo) {
+  .option("--lint", "Lint output (tslint), this may take a while")
+  .option("--swagger <path>", "Path to swagger yml, repeat to aggregate", function (val, memo) {
     memo.push(val);
     return memo;
   }, [])
+  .option("--dst <path>", "Destination path, default: same as the first swagger")
   .parse(process.argv);
 
+
+program.on('--help', function(){
+  console.log('');
+  console.log('  At least one swagger file is required');
+  console.log('  At least one TARGET is required');
+  console.log('');
+  console.log('  Examples:');
+  console.log('');
+  console.log('    nema --swagger=swagger-file.yml --mongoose --express --dst server/');
+  console.log('    nema --swagger=swagger-file.yml --angular5-api --dst angular/app/src/api/');
+  console.log('');
+});
+
+
 if (!program.swagger) {
-  red("--swagger [path] is required");
+  red("--swagger <path> is required");
+  program.help();
   process.exit(1);
 }
 
-// TODO add more targets!!
 if (!program.angular5Api && !program.mongoose && !program.express) {
-  red("no target to generate");
+  red("At least one TARGET is required");
+  program.help();
   process.exit(1);
 }
 
+
+// parse and aggregate definitions files
 
 let api: Api;
-let dstPath;
+let dstPath = program.dst;
 program.swagger.forEach((swagger) => {
   if (api) {
     api.aggregate(Api.parseSwaggerFile(swagger, !!program.mongoose), !!program.overrideMethods, !!program.overrideModels);
   } else {
     api = Api.parseSwaggerFile(swagger, !!program.mongoose);
-    dstPath = path.dirname(swagger);
+
+    if (!dstPath) {
+      dstPath = path.dirname(swagger);
+    }
   }
 });
-
-//console.log(api);
-//process.exit(0);
 
 const generators = [];
 
 // create all generators
 // some generator may modify api metadata
+
 if (program.angular5Api) {
-  green("Generating: Angular5");
+  green("Instancing generator: angular5-api");
   generators.push(new Angular5Client(dstPath, api));
 }
-if (program.mongoose) {
-  green("Generating: Mongoose");
-  generators.push(new Mongoose(dstPath, api));
-}
 if (program.express) {
-  green("Generating: Express");
+  green("Instancing generator: express");
   generators.push(new Express(dstPath, api));
 }
+// NOTE express need to be before mongoose
+if (program.mongoose) {
+  green("Instancing generator: mongoose");
+  generators.push(new Mongoose(dstPath, api));
+}
 
+green("Start generation");
 // then generate all with a common api source
 generators.forEach((g) => {
   g.generate(true, !!program.lint);
